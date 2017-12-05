8b483d8aa674fc1ebb609cca1df275858128a747
Dubina Dzmitry <ddubina@exadel.com>
2017-12-05 13:16:03 +0300

    1

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationsTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationsTests.java
@@ -14,10 +14,8 @@ public class GroupCreationsTests {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    WebDriver driver;
     System.setProperty("webdriver.gecko.driver", "C:\\Users\\ddubina\\Downloads\\geckodriver.exe");
     driver =new FirefoxDriver();
     baseUrl = "https://www.katalon.com/";
@@ -25,27 +23,40 @@ public class GroupCreationsTests {
   }
 
   @Test
-  public void GroupCreationsTests() throws Exception {
-    driver.get("http://localhost/addressbook/addressbook/group.php");
+  public void testUntitledTestCase() throws Exception {
+    driver.get("http://localhost/addressbook/addressbook/");
     driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test2");
+    driver.findElement(By.name("group_name")).sendKeys("1");
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("1");
+    driver.findElement(By.name("group_header")).sendKeys("2");
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("2");
+    driver.findElement(By.name("group_footer")).sendKeys("3");
     driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.linkText("group page")).click();
   }
 
-
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
 
   private boolean isElementPresent(By by) {
     try {

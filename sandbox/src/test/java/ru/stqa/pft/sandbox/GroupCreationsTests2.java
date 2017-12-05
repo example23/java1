6ce93b8deb86c720c9d8ac6fdca64370844c1ff8
Dubina Dzmitry <ddubina@exadel.com>
2017-12-05 15:24:04 +0300

    1

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/GroupCreationsTests2.java
@@ -0,0 +1,101 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class GroupCreationsTests2 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.gecko.driver", "C:\\Users\\ddubina\\Downloads\\geckodriver.exe");
+    driver =new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+  }
+
+  @Test
+  public void GroupCreationsTests2() throws Exception {
+    driver.get("http://localhost/addressbook/addressbook/");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=user | ]]
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.id("LoginForm")).click();
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("add new")).click();
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys("1");
+    driver.findElement(By.name("middlename")).click();
+    driver.findElement(By.name("middlename")).clear();
+    driver.findElement(By.name("middlename")).sendKeys("2");
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys("3");
+    driver.findElement(By.name("nickname")).click();
+    driver.findElement(By.name("nickname")).clear();
+    driver.findElement(By.name("nickname")).sendKeys("4");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.id("1")).click();
+    acceptNextAlert = true;
+    driver.findElement(By.xpath("//input[@value='Delete']")).click();
+    assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}

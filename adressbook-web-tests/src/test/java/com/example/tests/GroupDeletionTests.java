0a3cea2b03db3a3b33326af7ca7ff0b3560f8c46
Dubina Dzmitry <ddubina@exadel.com>
2017-12-05 16:00:02 +0300

    132

--- adressbook-web-tests/src/test/java/com/example/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/com/example/tests/GroupDeletionTests.java
@@ -1,12 +1,14 @@
-package ru.stqa.pft.adressbook;
+package com.example.tests;
 
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class GroupCreationsTests {
+public class TestCase1 {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -14,50 +16,27 @@ public class GroupCreationsTests {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty("webdriver.gecko.driver", "C:\\Users\\ddubina\\Downloads\\geckodriver.exe");
-    driver =new FirefoxDriver();
+    driver = new FirefoxDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    login("admin", "secret");
   }
 
-  private void login(String username, String password) {
+  @Test
+  public void testCase1() throws Exception {
     driver.get("http://localhost/addressbook/addressbook/");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.id("LoginForm")).click();
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  @Test
-  public void testUntitledTestCase() throws Exception {
-    GoToGroupsPage();
-    driver.findElement(By.name("new")).click();
-    FillGroupForm(new GroupData("1", "3"));
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
-  }
-
-  private void FillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("3");
-  }
-
-
-  private void GoToGroupsPage() {
     driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("selected[]")).click();
+    driver.findElement(By.name("delete")).click();
+    driver.findElement(By.linkText("group page")).click();
   }
 
   @AfterClass(alwaysRun = true)
---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupData.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.adressbook;
+
+public class GroupData {
+    private final String name;
+    private final String header;
+
+    public GroupData(String name, String header) {
+        this.name = name;
+        this.header = header;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+}
